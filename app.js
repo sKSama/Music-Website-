c5b0cfbfd8a81b12548d4743a05c8d6af37dab68
sKSama <63472170+sKSama@users.noreply.github.com>
2021-04-24 17:17:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -89,7 +89,9 @@ app.get("/loginfailed", function(req, res){
 });
 
 
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
-});
+//app.listen(3000, function(){
+ // console.log("Server started on port 3000.");
+//});
