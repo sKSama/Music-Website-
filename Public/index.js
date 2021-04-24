1b641c545ff6a1aaf4f28b532ef2756861ac464c
sKSama <63472170+sKSama@users.noreply.github.com>
2021-04-24 18:06:52 +0530

    Update index.js

--- Public/index.js
+++ Public/index.js
@@ -27,3 +27,16 @@ if(pass.value.length<4)
   alert("Registered! Login Now to Continue!");
   return true;
 }
+
+function ValidateLoginEmail(inputText)
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(!inputText.value.match(mailformat))
+{
+  alert("You have entered an invalid email address!");
+  document.login.email.focus();
+  return false;
+}
+
+  return true;
+}
